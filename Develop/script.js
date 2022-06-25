664b548e2692b0cb4d5b01b38852b52e34954ad1
mmtaylor <taylor.marymargaret@gmail.com>
2022-06-24 23:04:59 -0400

    created first prompt box- password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
@@ -11,11 +11,32 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
 
 function promptMe(){
-  let passwordLength = prompt("Choose a password length of at least 8 characters but no more than 128 characters"); 
-  alert (passwordLength)
+
+  let passwordLength = prompt("Choose a password length of at least 8 characters but no more than 128 characters");
+if (passwordLength>=8 && passwordLength<=128) {
+  promptMeAgain()
+} else {
+  window.alert("Invalid Entry")
+}
 }
 
-function generatePassword() 
+function promptMeAgain(){
+  
+}
+
+
+// promptMe()
+
+
+
+
+// if (passwordLength.text)
+
+
+
+
+
+
